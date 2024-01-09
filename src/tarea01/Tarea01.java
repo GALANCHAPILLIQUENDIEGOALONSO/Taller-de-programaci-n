830d66fae736d9b273ea7058e3f4fe0f9b0e8e21
Diego <Diego@DESKTOP-TLL3IAQ>
2024-01-08 21:57:04 -0500

    segundo commit

--- src/tarea01/Tarea01.java
+++ src/tarea01/Tarea01.java
@@ -4,6 +4,8 @@
  */
 package tarea01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Diego
@@ -15,6 +17,13 @@ public class Tarea01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double soles;
+        double dolares;
+        final double cambio=3.89;
+        
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("ingresar cantidad en soles: ");
+        
     }
     
 }
