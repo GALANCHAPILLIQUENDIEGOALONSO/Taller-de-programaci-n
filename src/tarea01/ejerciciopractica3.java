3bd15f9b5f1a13c516a6820f712cb42ed3840935
LAB-USR-LN6377-A0607 <LAB-USR-LN6377-A0607@UTP362631.grupoutp.net>
2024-02-01 14:25:11 -0500

    completar ejercicio3

---
+++ src/tarea01/ejerciciopractica3.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB-USR-LN6377-A0607
+ */
+public class ejerciciopractica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int color; 
+        int celeste=0,morado=0, azul=0;
+        Scanner lector = new Scanner (System.in);
+        System.out.println("elege un color: \n\tceleste[1] \n\tmorado[2] \n\tazul[3]");
+        for (int i= 1; i<=10; i++){
+            System.out.println("ingresa el color: ");
+            if
+              
+                
+        }
+    }
+    
+}
